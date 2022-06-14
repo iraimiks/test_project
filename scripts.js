c8766c6eb0c9f44b6f4b15fb9520ab2a5669bf99
Raimonds Bortascenoks <r.bortascenoks@gmail.com>
2022-06-14 16:59:01 +0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,84 @@
+const people = [
+  { name: 'adri'},
+  { name: 'becky'},
+  { name: 'chris'},
+  { name: 'dillon'},
+  { name: 'evan'},
+  { name: 'frank'},
+  { name: 'georgette'},
+  { name: 'hugh'},
+  { name: 'igor'},
+  { name: 'jacoby'},
+  { name: 'kristina'},
+  { name: 'lemony'},
+  { name: 'matilda'},
+  { name: 'nile'},
+  { name: 'ophelia'},
+  { name: 'patrick'},
+  { name: 'quincy'},
+  { name: 'roslyn'},
+  { name: 'solene'},
+  { name: 'timothy'},
+  { name: 'uff'},
+  { name: 'violet'},
+  { name: 'wyatt'},
+  { name: 'x'},
+  { name: 'yadri'},
+  { name: 'zack'},
+];
+
+let searchInputEl = document.getElementById("search");
+let listItems = document.getElementById("list-items");
+let clearBtn = document.getElementById("clear");
+
+
+clearBtn.addEventListener("click", () => {
+  clearListFromView();
+})
+
+
+searchInputEl.addEventListener('input', text => {
+  let valueInput = text.target.value;
+  //find element in array using filter()
+  valueInput = valueInput.toLowerCase();
+
+  try {
+    if(valueInput && valueInput.length > 0) {
+      setItemsToView(people.filter(persona => persona.name.includes(valueInput)));
+    } else {
+      clearListFromView();
+    }
+  } catch (error) {
+    console.error(error);
+  }
+
+})
+
+function setItemsToView(itemsFinded) {
+  clearListFromView();
+
+  for (const item of itemsFinded ) {
+    let itemEl = document.createElement("li");
+    let text = document.createTextNode(item.name);
+    itemEl.appendChild(text);
+    listItems.appendChild(itemEl);
+  }
+}
+
+
+function clearListFromView() {
+  while(listItems.firstChild) {
+    listItems.removeChild(listItems.firstChild);
+  }
+}
+
+
+
+let valueD = null;
+
+
+if(valueD) {
+  console.log('get data');
+} else {
+  console.log('something wrong');
+}
